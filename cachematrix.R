e09d181e69e8394d4727a5ba81b591d21bc53f35
Arti Tangri <artitangri@yahoo.com>
2015-04-23 14:00:53 -0700

    Added code for makeCacheMatrix and cacheSolve
    Added code for makeCacheMatrix and cacheSolve for the assignment
    submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## This file contains two functions for calculating and caching inverse of a matrix 
 
 
-## Write a short comment describing this function
+## makeCacheMatrix gets and sets the value of a matrix and its inverse in the environment
+makeCacheMatrix <- function(x = matrix()) 
+  {
+      ## Set the inverse to NULL initially to begin with
+      inverse <- NULL
+      ## Set the value of x and inverse to NULL in the calling environment of set 
+      set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+      }
+      ## Return the value of x
+      get <- function() x
+      ## Set inverse as passed in to the inverse variable in calling environment
+      setinverse <- function(inv) inverse <<- inv
+      ## Return cached value of inverse 
+      getinverse <- function() inverse
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## cacheSolve checks the cache for inverse of a matrix and calculates it if not found
+cacheSolve <- function(x, ...) 
+  {
+      ## Checks if inverse of x is cached in the environment 
+      inverse <- x$getinverse()
+      ## If inverse of x is found in cache
+      if(!is.null(inverse)) {
+        message("getting cached data")
+        ## Return the cached inverse of x and exit the function
+        return(inverse)
+      }
+      ## If inverse is not found in cache calculate it and set it in the cache
+      data <- x$get()
+      inverse <- solve(data)
+      x$setinverse(inverse)
+      ## Return the inverse of 'x'
+      inverse
 }
